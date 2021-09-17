94cb88acb88a22c2576c1eb814f668f48524931d
sseob2 <leesa@ssfs.org>
2021-09-16 20:21:00 -0400

    AverageRound

--- src/AverageRounded.java
+++ src/AverageRounded.java
@@ -5,6 +5,16 @@ public class AverageRounded {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter three positive decimal numbers: ");
+        double num1 = scan.nextDouble();
+        double num2 = scan.nextDouble();
+        double num3 = scan.nextDouble();
+
+        double total = num1 + num2 + num3;
+        double average = (total / 3) + 0.5;
+        int result = (int) average;
+
+        System.out.println("Average = " + result);
 
     }
 }
